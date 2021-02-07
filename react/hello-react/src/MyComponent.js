640d5107d7c89a8bcbe1327a93b1a58c0472f394
leejiwan <egg351@naver.com>
2021-02-07 22:00:11 +0900

    class 컴포넌트 update

--- react/hello-react/src/MyComponent.js
+++ react/hello-react/src/MyComponent.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 
-const MyComponent = () => {
-    return <div>"test Component"</div>
+const MyComponent = props => {
+    return <div>{props.name} <span>{props.children}</span></div>
 };
+
+MyComponent.defaultProps = {
+    name : 1234
+}
+
+MyComponent.propTypes = {
+    name : PropTypes.string
+}
 
 export default MyComponent;
