b2d4889fa0607e743c4ac2c96f7a221f313cb892
leejiwan <egg351@naver.com>
2021-04-22 23:17:52 +0900

    update

--- algorithm/8.js
+++ algorithm/8.js
@@ -1,8 +1,7 @@
 function solution(bridge_length, weight, truck_weights) {
-  var answer = 0;
-  
+  var answer = 1;
 
-  //첫번째
+  //첫번째 트럭 넣기
   var progress = [{
     weight: truck_weights.shift(),
     time: 0
@@ -12,38 +11,38 @@ function solution(bridge_length, weight, truck_weights) {
   do{
     answer++;
 
+    //시간증가
     for(var n in progress) {
       progress[n].time++;
     }
- 
-    console.log('proressSum::' + proressSum)
-    console.log(progress)
-
+     
+    //다리에 뺴기
     if(progress[0].time == bridge_length) {
       proressSum = proressSum - progress[0].weight;
       progress.shift();     
     }
-
+    
+    
     if(truck_weights.length > 0) {
-      if((proressSum) <= weight) {
-        proressSum =  proressSum + progress[0].weight;  
-        if(truck_weights.length > 0) {
-          progress.push({
-            weight: truck_weights.shift(),
-            time: 0
-          })
-        }   
+      //다리에 넣기
+      if((proressSum + truck_weights[0]) <= weight) {
+        proressSum =  proressSum + truck_weights[0];  
+        progress.push({
+          weight: truck_weights.shift(),
+          time: 0
+        })
       }
     }
-    
+  //  console.log('-------------------------')
+   // console.log(progress)
+   // console.log('proressSum::' + proressSum)
+   
   }while(proressSum != 0)
-
-  console.log('answer::' + answer)
   return answer;
 }
-var bridge_length = 2;	
-var weight = 10;
-var truck_weights = [7,4,5,6];
+var bridge_length = 100;	
+var weight = 100;
+var truck_weights = [10];
 solution(bridge_length, weight, truck_weights);
 //2	10	[7,4,5,6] 8
 //100	100	[10] 101
