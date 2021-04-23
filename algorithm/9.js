bd959f404aea5722582c350e9325e72c1f6b3f14
leejiwan <egg351@naver.com>
2021-04-23 15:12:45 +0900

    update

---
+++ algorithm/9.js
@@ -0,0 +1,36 @@
+function solution(progresses, speeds) {
+  var answer = [];
+  var result = [];
+  for(var i in progresses) {
+    result.push(Math.ceil((100 - progresses[i]) * (1 / speeds[i])))
+  }
+ 
+  var before = result[0];
+  var index = 0;
+  for(var i in result) {
+    if(before >= result[i]) {
+      index++;
+    }
+
+    if(before < result[i]) {
+      answer.push(index);
+      before = result[i];
+      index = 1;
+    }
+
+    if(i == result.length-1) {
+      answer.push(index);
+    }
+
+    console.log('result::' + result[i])
+    console.log('index::' + index);
+    console.log('before::' + before);
+
+
+  }
+  return answer;
+
+}
+//var progresses = [93, 30, 55]; var speeds = [1, 30, 5];;	//[7,3,9] [2, 1]
+var progresses = [95, 90, 99, 99, 80, 99];	var speeds = [1, 1, 1, 1, 1, 1];	//   [1, 3, 2]
+solution(progresses, speeds)
