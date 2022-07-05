19a8e1914c017e20a2cc028ac9db8da7f89d3cd0
leejiwan <egg351@naver.com>
2022-07-05 21:56:48 +0900

    update

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -1,12 +1,16 @@
-/* eslint-disable */
 import './App.css';
-import {Button} from 'react-bootstrap'
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <div>asdsadasdad</div>
-      <Button variant="outline-light">Light</Button>{' '}
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
     </div>
   );
 }
