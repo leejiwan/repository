2d16d72eeff1fdb608d3a8aaf7089422aaf22edd
leejiwan <egg351@naver.com>
2022-07-06 21:30:28 +0900

    update

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -1,16 +1,45 @@
+/* eslint-disable */
 import './App.css';
-import { Button } from 'react-bootstrap';
+import {Container,Nav,Navbar,Row,Col} from 'react-bootstrap';
+import bgImg from './logo.svg';
 
+
+//public 폴더안은 변하지 않음
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>{' '}
-      <Button variant="secondary">Secondary</Button>{' '}
-      <Button variant="success">Success</Button>{' '}
-      <Button variant="warning">Warning</Button>{' '}
-      <Button variant="danger">Danger</Button>{' '}
-      <Button variant="info">Info</Button>{' '}
-      <Button variant="light">Light</Button>{' '}
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={process.env.PUBLIC_URL +  "/logo192.png"} width="30" height="30"  className="d-inline-block align-top"/>
+            Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="main-bg" style={{backgroundImage : 'url('+ bgImg +')'}}></div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" style={{height : '200px', width : '100px'}}/>
+            <h4>상품명1</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+          <img src="https://codingapple1.github.io/shop/shoes2.jpg" style={{height : '200px', width : '100px'}}/>
+            <h4>상품명1</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+          <img src="https://codingapple1.github.io/shop/shoes3.jpg" style={{height : '200px', width : '100px'}}/>
+            <h4>상품명1</h4>
+            <p>상품설명</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
