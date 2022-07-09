47feb7100431e6a5f5b9b7535d6c56455d0b4321
leejiwan <egg351@naver.com>
2022-07-09 16:11:56 +0900

    update

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -1,17 +1,20 @@
 /* eslint-disable */
 import './App.css';
-import {Container,Nav,Navbar,Row,Col} from 'react-bootstrap';
+import { Container, Nav, Navbar, Row, Col } from 'react-bootstrap';
 import bgImg from './logo.svg';
-
-
+import { useState } from 'react';
+import { product, name } from './data.js' //import 문법
+import { Route, Routes, Link } from 'react-router-dom'
 //public 폴더안은 변하지 않음
 function App() {
+
+  let [prod] = useState(product);
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">
-            <img src={process.env.PUBLIC_URL +  "/logo192.png"} width="30" height="30"  className="d-inline-block align-top"/>
+            <img src={process.env.PUBLIC_URL + "/logo192.png"} width="30" height="30" className="d-inline-block align-top" />
             Shop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
@@ -20,26 +23,37 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
-      <div className="main-bg" style={{backgroundImage : 'url('+ bgImg +')'}}></div>
+      <Link to='/'>home</Link>
+      <Routes>
+        <Route path='/detail' element={<div>상세페이쥐</div>} />
+        <Route path='/about' element={<div>어바웃</div>} />
+        <Route path='/' element={<div>main</div>} />
+        <Route />
+      </Routes>
+      <div className="main-bg" style={{ backgroundImage: 'url(' + bgImg + ')' }}></div>
       <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-          <img src="https://codingapple1.github.io/shop/shoes1.jpg" style={{height : '200px', width : '100px'}}/>
-            <h4>상품명1</h4>
-            <p>상품설명</p>
-          </div>
-          <div className="col-md-4">
-          <img src="https://codingapple1.github.io/shop/shoes2.jpg" style={{height : '200px', width : '100px'}}/>
-            <h4>상품명1</h4>
-            <p>상품설명</p>
-          </div>
-          <div className="col-md-4">
-          <img src="https://codingapple1.github.io/shop/shoes3.jpg" style={{height : '200px', width : '100px'}}/>
-            <h4>상품명1</h4>
-            <p>상품설명</p>
-          </div>
-        </div>
+        <ShoesData />
       </div>
+    </div>
+  );
+}
+
+function ShoesData() {
+  debugger;
+  let [prod] = useState(product);
+  return (
+    <div className="row">
+      {
+        prod.map((data, index) => {
+          return (
+            <div className="col-md-4" key={index}>
+              <img src={data.url} style={{ height: '200px', width: '100px' }} />
+              <h4>{data.title}</h4>
+              <p>{data.content}</p>
+            </div>
+          );
+        })
+      }
     </div>
   );
 }
