962d9a53df30e9237990c6fd1afe2127e86ef234
leejiwan <egg351@naver.com>
2022-07-12 22:16:29 +0900

    styled-components

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 import { Container, Nav, Navbar, Row, Col } from 'react-bootstrap';
 import bgImg from './logo.svg';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { product, name } from './data.js' //import 문법
 import { Route, Routes, Link, useParams, useNavigate, Outlet } from 'react-router-dom'
 //public 폴더안은 변하지 않음
+import styled from 'styled-components'
+
 function App() {
   let [prod] = useState(product);
   //hook 유용한 함수..?
@@ -26,7 +28,7 @@ function App() {
       </Navbar>
       <Routes>
         <Route path='/about/' element={<About />} >
-          <Route path='member' element={<div>zzzz</div>} /> {//nested routes 여러 유사한 페이지 필요}
+          <Route path='member' element={<div>zzzz</div>} /> {/*nested routes 여러 유사한 페이지 필요*/}
         </Route>
         <Route path='/detail/:id' element={<Detail data={prod} />} />
         <Route path='/' element={<ShoesData data={prod} />} />
@@ -59,12 +61,59 @@ function ShoesData(props) {
   );
 }
 
+
+/*
+lifeCycle
+mount 페이지 장착
+update 업데이트
+unmount 제거
+*/
+/*
+class Detail2 extends React.Component {
+  componentDidMount() {
+    //mount 페이지 장착
+  }
+  componentDidUpdate() {
+    //update 업데이트
+  }
+  componentWillUnmount() {
+    //unmount 제거
+  }
+
+}
+*/
 function Detail(props) {
+  useEffect(() => {
+    /*mount,update 코드 실행
+      html 렌더링 완료 후 실행
+      오래걸리는 작업(연산, 타이머, 서버에서 데이터 가져오는 작업)
+    */
+    /*
+     Todo 페이지 방문 후 2초 지나면 div 숨기기
+    */
+    console.log('start')
+  });
+
+  let [count, setCount] = useState(0);
+
   let id = useParams();
+  let Yellowbtn = styled.button`
+  backgound : ${props => props.bg};
+  color : black;
+  `;
+  let Box = styled.div`
+  padding : 50px;
+  color : grey;
+  backgound: ${props => props.bg == 'red' ? 'red' : 'blue'};
+`;
   debugger;
   return (
     <div className="container">
       <div className="row">
+        <Yellowbtn bg='red' onClick={() => {
+          setCount(count + 1);
+        }}>button</Yellowbtn>
+        <Box>zzzz</Box>
         <div className="col-md-6">
           <img src={props.data[id.id].url} width="100%" />
         </div>
