8e9d87078bd914505ed6382376a8d4e576b8c94e
lee <egg351@naver.com>
2022-08-04 20:51:20 +0900

    update

--- react/shop/src/Cart.js
+++ react/shop/src/Cart.js
@@ -1,33 +1,40 @@
-import { Table } from 'react-bootstrap'
-
+import { Table } from "react-bootstrap";
+//redux
+import { useSelector } from "react-redux";
 /*
 Redux
 */
 function Cart() {
-
-
-    return (
-        <div>
-            <Table>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>상품명</th>
-                        <th>수량</th>
-                        <th>변경하기</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>안녕</td>
-                        <td>안녕</td>
-                        <td>안녕</td>
-                    </tr>
-                </tbody>
-            </Table>
-        </div>
-    )
+  let a = useSelector(state => {
+    return state;
+  });
+  console.log(a.prod);
+  return (
+    <div>
+      <Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>상품명</th>
+            <th>수량</th>
+            <th>변경하기</th>
+          </tr>
+        </thead>
+        <tbody>
+          {a.prod.map((data, index) => {
+            return (
+              <tr>
+                <td>{data.id}</td>
+                <td>{data.name}</td>
+                <td>{data.count}</td>
+                <td>안녕</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </div>
+  );
 }
 
-export default Cart
+export default Cart;
--- react/shop/src/store.js
+++ react/shop/src/store.js
@@ -4,12 +4,20 @@ import { configureStore, createSlice } from "@reduxjs/toolkit";
 */
 
 let user = createSlice({
-    name: "user",
-    initialState: "kim",
+  name: "user",
+  initialState: "kim",
+});
+let prod = createSlice({
+  name: "prod",
+  initialState: [
+    { id: 0, name: "nike", count: 2 },
+    { id: 1, name: "adidas", count: 1 },
+  ],
 });
 
 export default configureStore({
-    reducer: {
-        user: user.reducer,
-    },
+  reducer: {
+    user: user.reducer,
+    prod: prod.reducer,
+  },
 });
