313b27c8ebc098a6974e3034844c78b05c891324
leejiwan <egg351@naver.com>
2022-09-03 23:17:23 +0900

    Update 17.js

--- algorithm/17.js
+++ algorithm/17.js
@@ -1,18 +1,22 @@
+//LV.2016년
 function solution(a, b) {
     var answer = [];
     var week = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU'];
     //2016년 1월 1일 금요일
     //2016년 5월 24일 화요일
+    var month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    var bDay = 0;
 
-    var stDate = new Date(2016, 01, 01);
-    var endDate = new Date(2016, 05, 20);
-
-    var btMs = endDate.getTime() - stDate.getTime();
-    var btDay = btMs / (1000 * 60 * 60 * 24);
-    console.log(btDay)
-    console.log(btDay % 7)
-    console.log(week[btDay % 7])
-    return week[btDay % 7];
+    if (a == 1) {
+        bDay = b;
+    } else {
+        for (var i = 0; i < a; i++) {
+            bDay = bDay + month[i];
+        }
+        bDay = bDay + b;
+    }
+    bDay = bDay - 1;
+    return week[bDay % 7];
 }
 
-solution()
+solution(1, 6);
