b41a65b835d7679f649b2b06f9b3197674d7955d
leejiwan <egg351@naver.com>
2022-09-11 17:09:22 +0900

    Create 18.js

---
+++ algorithm/18.js
@@ -0,0 +1,37 @@
+//올바른 괄호
+function solution(s) {
+    var answer = true;
+    var ch1 = 0;
+    var ch2 = 0;
+    var arr = [...s];
+    var len = arr.length;
+    var i = 0;
+
+
+
+    if (arr[0] == '(' && arr[len - 1] == ')') {
+        while (len > i) {
+            if (arr[i] == '(') {
+                ch1++;
+            } else {
+                ch2++;
+            }
+            i++;
+            if (ch1 - ch2 < 0) {
+                break;
+                return false;
+            }
+        }
+
+        if (ch1 == ch2) {
+            answer = true;
+        } else {
+            answer = false;
+        }
+
+    } else {
+        answer = false;
+    }
+    console.log(answer)
+    return answer;
+}
