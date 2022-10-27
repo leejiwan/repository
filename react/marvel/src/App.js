aef3fc83a55fdc283e69e356b6124506893c46c9
leejiwan <egg351@naver.com>
2022-10-27 22:50:49 +0900

    fixed 추가

--- react/marvel/src/App.js
+++ react/marvel/src/App.js
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
         <Container>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
