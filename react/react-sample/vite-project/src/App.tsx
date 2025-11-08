ae38a776ca66d5690b5a75b577d60e008be422f6
lee <egg351@naver.com>
2025-11-08 20:43:52 +0900

    테스트

--- react/react-sample/vite-project/src/App.tsx
+++ react/react-sample/vite-project/src/App.tsx
@@ -1,15 +1,41 @@
 import { useState, useEffect } from 'react'
+import { ts, apiKey, privateKey, mavelAddress} from  "./Const.ts";
+import { AxiosResponse } from 'axios';
+import md5 from "md5";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+interface MarvelParams {
+  ts: string;
+  apikey: string;
+  hash: string;
+}
+
 function App() {
+  const paramObj: MarvelParams = {
+  ts,
+  apikey: apiKey,
+  hash: md5(ts + privateKey + apiKey)
+};
+
   const [count, setCount] = useState(0) // 상태변경
 
 useEffect(() => {
-  alert("리액트");
-}, []);
+      axios({
+        method: "GET",
+        url: mavelAddress + "characters/1",
+        params: paramObj,
+      })
+      .then((response: AxiosResponse) => {
+       
+      })
+      .catch(res => {
+        alert('status::' + res.response.request.status + '\n' + 'statusText::' + res.response.request.statusText);
+      });
 
+}, [count]);
+  
   return (
     <>
       <div>
