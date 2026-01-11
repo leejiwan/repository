2bd641a93d9c487231f70cc6db2950cc7b30426c
lee <egg351@naver.com>
2026-01-11 14:47:04 +0900

    리액트 리마인드

--- react/onbite-react/section04/src/component/Register.jsx
+++ react/onbite-react/section04/src/component/Register.jsx
@@ -1,19 +1,46 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 /*
   1. 이름
   2. 생년월일  
   3. 국적
 */
 const Register = () => {
-    const [name, setName] = useState("");
-    const onChangeName = (e) => {
-        setName(e.target.value)
+    //useRef 컴포넌트 내부 변수이지만 리렌더링을 유발X
+    const inputRef = useRef(0);
+
+    const [input, setInput] = useState({
+        name :"",
+        birth : "",
+        country : "",
+        bio : ""
+    });
+
+    const onChange = (e) => {
+        setInput({
+            ...input,
+            [e.target.value] : e.target.value
+        })
+    }
+    const onSubmit = () => {
+        if(input.name === "") {
+            ///
+            console.log(inputRef.current)
+        }
     }
     return(        
-        <div>
-            <input onChange={onChangeName} placeholder={"이름"}></input>
-            <input type="date"></input>
-        </div>
+        <>
+            <div>
+                <input ref={inputRef} onChange={onChange} placeholder={"이름"}></input>
+                <input type="date"></input>
+            </div>
+            <div>
+                <select value = {input.country} onChange={onChange}>
+                    <option>한국</option>
+                    <option>중국</option>    
+                </select>
+                {input.country}
+            </div>
+        </>       
     ) 
 }
 
